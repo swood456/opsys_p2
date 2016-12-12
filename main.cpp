3b5ddd418cdd9f1c65b871f0e7ecc54b76e89530
Jason Kraft <kraftj3@rpi.edu>
2016-12-12 18:30:34 -0500

    fixed bug with opt

--- main.cpp
+++ main.cpp
@@ -740,7 +740,7 @@ void VirtualOPT(std::vector<int> pageRefs){
 
 	for(unsigned int i = 0; i < pageRefs.size(); i++){
 		replaceIndex = 0;
-		furthestDistance = 0;
+		furthestDistance = -1;
 		unsigned int j;
 
 		//see if the current thing is already in there
@@ -791,7 +791,8 @@ void VirtualOPT(std::vector<int> pageRefs){
 						break;
 					}
 				}
-
+				// if (k + i + 1 == pageRefs.size())
+				// 	k = 0;
 				if (furthestDistance < k || (furthestDistance == k && virtualMemory[j] < virtualMemory[replaceIndex])) {
 					furthestDistance = k;
 					replaceIndex = j;
