0762e8181b046d63dec5c711c5eb1fa3efba3cfe
swood456 <crazedsportsfan@gmail.com>
2016-12-12 10:08:10 -0500

    parsed input file for contiguous and non, need to determine data structure for storing that and how we are storing things in memory

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
+#include <fstream>
+#include <string>
+#include <cstring>
+
+const int t_memmove = 1; //time to move 1 frame of memory in defrag
 
 void printMemoryDiagram(std::vector<char> memory){
 	//first, print the top line
@@ -24,9 +29,65 @@ int main(int argc, char* argv[]){
 	std::cout << "hello world" <<std::endl;
 
 	//check command line arguments
+	//will be in form of ./a.out memory_management_file virtual_file
 
 	//parse input file
-	//file format is NAME NUM_FRAMES ARRIVALTIME_0/RUNTIME_0 ARRIVALTIME_1/RUNTIME_1 ...
+	std::ifstream inFile;
+	inFile.open(argv[1]);
+
+	if(!inFile){
+		fprintf(stderr, "ERROR: invalid file for Contiguous and Non-Contiguous memory\n");
+		return EXIT_FAILURE;
+	}
+	
+
+	std::string line;
+
+	//get the number of processes
+	int numProcesses;
+	std::getline(inFile, line);
+	numProcesses = atoi(line.c_str());
+
+	char processName;
+	int numMemFrames;
+	//std::vector<std::pair<int, int> > arrivalRunTimes (numProcesses);
+
+	while(std::getline(inFile, line)){
+		if(line[0] != '#' && line[0] != ' ' && line[0] != '	' && line[0] != '\n' && line[0] != '\0'){
+			
+			//std::cout << "got line " << line << std::endl;
+			
+			processName = line[0];
+
+			line.erase(0,2);
+			//get numMemFrames;
+
+			char * token = strtok(const_cast<char*>(line.c_str()), " ");
+
+			numMemFrames = atoi(token);
+
+			std:: cout << "process name " << processName << " numMemFrames = " << numMemFrames << std::endl;
+
+			int arrivalTime;
+			int runTime;
+			//go until we have all the process pair things
+			token = strtok(NULL, " /");
+			while(token != NULL){
+				arrivalTime = atoi(token);
+				//std::cout << " token: |" << token <<"|\n";
+				token = strtok(NULL, " /");
+				runTime = atoi(token);
+
+				std::cout << " arrival time: " << arrivalTime << " run time: " << runTime << std::endl;
+
+
+				token = strtok(NULL, " /");
+
+			}
+
+		}
+	}
+
 
 	//notes start on 11-14
 
